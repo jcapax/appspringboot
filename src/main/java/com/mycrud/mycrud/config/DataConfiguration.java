b23a521c54ca75e4497bde7c0771686ae3a4f82d
juan carlos <jcapax@gmail.com>
2019-02-19 15:01:58 -0400

    app configurada con postgress para heroku

--- src/main/java/com/mycrud/mycrud/config/DataConfiguration.java
+++ src/main/java/com/mycrud/mycrud/config/DataConfiguration.java
@@ -1,7 +1,11 @@
 package com.mycrud.mycrud.config;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -12,26 +16,42 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 @Configuration
 public class DataConfiguration {
 	
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
-		dataSource.setUrl("jdbc:mariadb://localhost/app_crud");
-		dataSource.setUsername("root");
-		dataSource.setPassword("mariaroot");
-		
-		return dataSource;
-	}
+//	@Bean
+//	public DataSource dataSource() {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+//		dataSource.setUrl("jdbc:mariadb://localhost/app_crud");
+//		dataSource.setUsername("root");
+//		dataSource.setPassword("mariaroot");
+//		
+//		return dataSource;
+//	}
+//	
+//	@Bean
+//	public JpaVendorAdapter jpaVendorAdapter() {
+//		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+//		adapter.setDatabase(Database.MYSQL);
+//		adapter.setShowSql(true);
+//		adapter.setGenerateDdl(true);
+//		adapter.setDatabasePlatform("org.hibernate.dialect.MariaDB53Dialect");
+//		adapter.setPrepareConnection(true);
+//		
+//		return adapter;
+//	}
 	
 	@Bean
-	public JpaVendorAdapter jpaVendorAdapter() {
-		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.MYSQL);
-		adapter.setShowSql(true);
-		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.MariaDB53Dialect");
-		adapter.setPrepareConnection(true);
-		
-		return adapter;
-	}
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 }
